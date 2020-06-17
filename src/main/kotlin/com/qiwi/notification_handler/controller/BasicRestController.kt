214b878da5384327c0db5051678446309a5c2910
Mindibaev Timofei <t.mindibaev@qiwi.com>
2020-06-17 18:48:32 +0300

    specify healthcheck mapping

--- src/main/kotlin/com/qiwi/notification_handler/controller/BasicRestController.kt
+++ src/main/kotlin/com/qiwi/notification_handler/controller/BasicRestController.kt
@@ -1,10 +1,12 @@
 package com.qiwi.notification_handler.controller
 
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class BasicRestController {
-    @GetMapping
-    fun healthCheck() = "OK";
+
+    @GetMapping(value = ["/healthcheck"])
+    fun healthCheck(): ResponseEntity<String> = ResponseEntity.ok("OK")
 }
