9c27ce8e65f9d37654747ce21bc9073146939fa6
Mindibaev Timofei <t.mindibaev@qiwi.com>
2020-06-17 17:51:46 +0300

    initial commit

---
+++ src/main/kotlin/com/qiwi/notification_handler/NotificationHandlerApplication.kt
@@ -0,0 +1,11 @@
+package com.qiwi.notification_handler
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotificationHandlerApplication
+
+fun main(args: Array<String>) {
+	runApplication<NotificationHandlerApplication>(*args)
+}
---
+++ src/test/kotlin/com/qiwi/notification_handler/NotificationHandlerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.qiwi.notification_handler
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotificationHandlerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
